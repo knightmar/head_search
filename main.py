290d3080ca9a2f54c56cfdb331f31665c761e5ed
knightmar <74785484+knightmar@users.noreply.github.com>
2022-02-15 18:09:18 +0100

    fix border

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import io
 import tkinter as tk
 import urllib.request
-from tkinter import ttk
+from tkinter.ttk import *
 
 from PIL import ImageTk, Image
 
@@ -23,23 +23,25 @@ class App(tk.Tk):
         self.pseudo = ""
         self.head = None
 
-        self.entry = ttk.Entry(self, text="Pseudo")
-        self.entry.pack()
-        self.confirm = ttk.Button(self, text="Confirm", command=self.confirm)
-        self.confirm.pack()
+        self.root = Frame(self).grid(padx=(100, 100), pady=(10, 10))
+
+        self.entry = Entry(self.root, text="Pseudo")
+        self.entry.grid(pady=(10, 10))
+
+        self.confirm = Button(self.root, text="Confirm", command=self.confirm)
+        self.confirm.grid(pady=(10, 10))
+
+        fond = tk.PhotoImage(file='fond.png')
+        self.imagelab = tk.Label(self, image=fond)
+        self.imagelab.grid(pady=(0, 10))
 
     def confirm(self):
         self.pseudo = self.entry.get()
         self.url = "https://minotar.net/avatar/" + self.pseudo
         self.head = WebImage(self.url).get()
-        self.imagelab = tk.Label(self, image=self.head)
-        self.imagelab.pack()
+        self.imagelab.configure(image=self.head)
 
 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-
-    def get(self):
-        return self.image
